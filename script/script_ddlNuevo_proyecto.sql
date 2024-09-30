5cf8766c65949b5ea67144ce588d7d82e8daf199
EnzoBarrios <Enzobarrios2018@gmail.com>
2024-09-30 05:10:46 -0300

    Modificacion de las restricciones de las tablas y se agrego un lote de datos representativos

--- script/script_ddlNuevo_proyecto.sql
+++ script/script_ddlNuevo_proyecto.sql
@@ -1,30 +1,35 @@
 --Creacion de base de datos--
 CREATE DATABASE GestionHotel;
+GO
 USE GestionHotel;
+GO
 
 --Creacion de tablas--
+
+-- Tabla Huesped
 CREATE TABLE Huesped
 (
+  dni INT NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
-  dni INT NOT NULL,
   CONSTRAINT PK_huesped PRIMARY KEY (dni),
-  CONSTRAINT CK_dniHuesped CHECK (dni <= 99999999)
+  CONSTRAINT CK_dniHuesped CHECK (dni BETWEEN 1 and 99999999) -- El dni del huesped no puede ser un valor negativo ni mayor a 8 digitos
 );
 
+-- Tabla Reserva
 CREATE TABLE Reserva
 (
   id_reserva INT NOT NULL,
-  fechaEntrada DATE NOT NULL,
-  fechaSalida INT NOT NULL,
+  fechaEntrada DATE NOT NULL CONSTRAINT DF_fechaEntrada DEFAULT GETDATE(), -- -- La fecha de entrada del huésped se establece por defecto como la fecha actual
+  fechaSalida DATE, -- La fecha de salida del huesped es opcional, si es que el huesped no reservo la habitación por un tiempo fijo
   dni INT NOT NULL,
   CONSTRAINT PK_reserva PRIMARY KEY (id_reserva),
-  CONSTRAINT FK_huesped_reseva FOREIGN KEY (dni) REFERENCES Huesped(dni),
-  CONSTRAINT CK_fecha CHECK (fechaInicio <= fechaFin),
-  CONSTRAINT CK_dniHuesped CHECK (dni <= 99999999)
+  CONSTRAINT FK_huesped_reserva FOREIGN KEY (dni) REFERENCES Huesped(dni),
+  CONSTRAINT CK_fecha CHECK (fechaEntrada <= fechaSalida)
 );
 
-CREATE TABLE _Piso
+-- Tabla Piso
+CREATE TABLE Piso
 (
   id_piso INT NOT NULL,
   nroPiso INT NOT NULL,
@@ -32,6 +37,7 @@ CREATE TABLE _Piso
   CONSTRAINT CK_nroPiso CHECK (nroPiso >= 0)
 );
 
+-- Tabla Pago
 CREATE TABLE Pago
 (
   id_pago INT NOT NULL,
@@ -40,6 +46,7 @@ CREATE TABLE Pago
   CONSTRAINT CK_importePago CHECK(importe > 0)
 );
 
+-- Tabla Metodo_pago
 CREATE TABLE Metodo_pago
 (
   id_metodoPago INT NOT NULL,
@@ -50,6 +57,7 @@ CREATE TABLE Metodo_pago
   CONSTRAINT CK_tipoPago CHECK(tipo in ('Debito', 'Credito', 'Efectivo'))
 );
 
+-- Tabla Habitacion
 CREATE TABLE Habitacion
 (
   id_habitacion INT NOT NULL,
@@ -62,17 +70,19 @@ CREATE TABLE Habitacion
   CONSTRAINT CK_costoHabitacion CHECK(costo > 0)
 );
 
+-- Tabla Detalle_reserva
 CREATE TABLE Detalle_reserva
 (
-  totalReserva INT NOT NULL,
   id_reserva INT NOT NULL,
   id_pago INT NOT NULL,
+  totalReserva INT NOT NULL,
   CONSTRAINT PK_detalle_reserva PRIMARY KEY (id_reserva, id_pago),
   CONSTRAINT FK_reserva_detalle_reserva FOREIGN KEY (id_reserva) REFERENCES Reserva(id_reserva),
   CONSTRAINT FK_pago_detalle_reserva FOREIGN KEY (id_pago) REFERENCES Pago(id_pago),
   CONSTRAINT CK_totalReserva CHECK (totalReserva > 0)
 );
 
+-- Tabla Detalle_habitacion
 CREATE TABLE Detalle_habitacion
 (
   id_habitacion INT NOT NULL,
