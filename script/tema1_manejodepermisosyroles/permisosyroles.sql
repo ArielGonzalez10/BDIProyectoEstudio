25f4809038a017fb5079a27e25bcb7dce96b0196
Ariel Gonzalez <arielgonzalezr9@gmail.com>
2024-10-31 12:15:36 -0300

    Teoria de permisos y roles

--- script/tema1_manejodepermisosyroles/permisosyroles.sql
+++ script/tema1_manejodepermisosyroles/permisosyroles.sql
@@ -8,11 +8,16 @@ CREATE ROLE rolUsuario;
 
 --Se asigna permisos de administrador al rol Administrador
 GRANT ALTER,DELETE,INSERT,SELECT,UPDATE TO rolAdministrador;
+GRANT EXECUTE ON sp_insertar_huesped TO rolAdministrador;
+GRANT EXECUTE ON sp_modificar_huesped TO rolAdministrador;
+GRANT EXECUTE ON sp_borrar_huesped TO rolAdministrador;
+GRANT EXECUTE ON fn_total_pagos TO rolAdministrador;
+GRANT EXECUTE ON fn_contar_huespedes TO rolAdministrador;
 
 --Se asgina permiso de solo lectura al rol Usuario y ejecucion de funcion almacenada
 GRANT SELECT ON Reserva TO rolUsuario;
 GRANT SELECT ON Detalle_Reserva TO rolUsuario;
-GRANT EXECUTE ON funcionAlmacenada TO rolUsuario
+
 
 --USUARIOS DE BASES DE DATOS
 --Creacion de inicios de sesion
